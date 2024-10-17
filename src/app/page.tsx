51ebb9bda3d0a47942b6c75b5276d85b618c08ee
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-10-17 16:35:01 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,8 @@ import { Metadata } from "next";
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
 
 export const metadata: Metadata = {
-  title:
-    "Next.js E-commerce Dashboard | TailAdmin - Next.js Dashboard Template",
-  description: "This is Next.js Home for TailAdmin Dashboard Template",
+  title: " Blocktools | The Ultimate Solana & EVM Chains Token & Airdrop Powerhouse Effortlessly",
+  description: "The Ultimate Solana & EVM Chains Token & Airdrop Powerhouse Effortlessly",
 };
 
 export default function Home() {
