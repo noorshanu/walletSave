9e1c7fe853e76f3c2fc13ee4af80f20b103710fb
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-10-19 02:29:31 +0530

    Update page.tsx

--- src/app/token-list/page.tsx
+++ src/app/token-list/page.tsx
@@ -4,23 +4,108 @@ import DefaultLayout from "@/components/Layouts/DefaultLayout";
 import React, { useState } from "react";
 import { FaList } from "react-icons/fa6";
 import { RiCheckboxCircleFill, RiCloseCircleFill } from "react-icons/ri";
+import { RiExpandUpDownFill } from "react-icons/ri";
 
 // Dummy Data
 const dummyTokens = [
-  { id: 1, name: "Bitcoin", chain: "BTC", address: "0123456789abcdef0123456789abcdef01234567", verified: true },
-  { id: 2, name: "Ethereum", chain: "ETH", address: "0123456789abcdef0123456789abcdef01234567", verified: false },
-  { id: 3, name: "Cardano", chain: "ADA", address: "0123456789abcdef0123456789abcdef01234567", verified: true },
-  { id: 4, name: "Solana", chain: "SOL", address: "0123456789abcdef0123456789abcdef01234567", verified: false },
-  { id: 5, name: "Polkadot", chain: "DOT", address: "0123456789abcdef0123456789abcdef01234567", verified: true },
-  { id: 6, name: "Solana", chain: "SOL", address: "0123456789abcdef0123456789abcdef01234567", verified: false },
-  { id: 7, name: "USD Coin", chain: "USDC", address: "0123456789abcdef0123456789abcdef01234567", verified: true },
-  { id: 8, name: "Solana", chain: "SOL", address: "0123456789abcdef0123456789abcdef01234567", verified: false },
-  { id: 9, name: "USD Coin", chain: "USDC", address: "0123456789abcdef0123456789abcdef01234567", verified: true },
-  { id: 10, name: "Polkadot", chain: "DOT", address: "0123456789abcdef0123456789abcdef01234567", verified: false },
-  { id: 11, name: "Solana", chain: "SOL", address: "0123456789abcdef0123456789abcdef01234567", verified: true },
-  { id: 12, name: "Ethereum", chain: "ETH", address: "0123456789abcdef0123456789abcdef01234567", verified: false },
-  { id: 13, name: "Ethereum", chain: "ETH", address: "0123456789abcdef0123456789abcdef01234567", verified: true },
-  { id: 14, name: "Ethereum", chain: "ETH", address: "0123456789abcdef0123456789abcdef01234567", verified: false },
+  {
+    id: 1,
+    name: "Bitcoin",
+    chain: "BTC",
+    address: "0123456789abcdef0123456789abcdef01234567",
+    verified: true,
+  },
+  {
+    id: 2,
+    name: "Ethereum",
+    chain: "ETH",
+    address: "0123456789abcdef0123456789abcdef01234567",
+    verified: false,
+  },
+  {
+    id: 3,
+    name: "Cardano",
+    chain: "ADA",
+    address: "0123456789abcdef0123456789abcdef01234567",
+    verified: true,
+  },
+  {
+    id: 4,
+    name: "Solana",
+    chain: "SOL",
+    address: "0123456789abcdef0123456789abcdef01234567",
+    verified: false,
+  },
+  {
+    id: 5,
+    name: "Polkadot",
+    chain: "DOT",
+    address: "0123456789abcdef0123456789abcdef01234567",
+    verified: true,
+  },
+  {
+    id: 6,
+    name: "Solana",
+    chain: "SOL",
+    address: "0123456789abcdef0123456789abcdef01234567",
+    verified: false,
+  },
+  {
+    id: 7,
+    name: "USD Coin",
+    chain: "USDC",
+    address: "0123456789abcdef0123456789abcdef01234567",
+    verified: true,
+  },
+  {
+    id: 8,
+    name: "Solana",
+    chain: "SOL",
+    address: "0123456789abcdef0123456789abcdef01234567",
+    verified: false,
+  },
+  {
+    id: 9,
+    name: "USD Coin",
+    chain: "USDC",
+    address: "0123456789abcdef0123456789abcdef01234567",
+    verified: true,
+  },
+  {
+    id: 10,
+    name: "Polkadot",
+    chain: "DOT",
+    address: "0123456789abcdef0123456789abcdef01234567",
+    verified: false,
+  },
+  {
+    id: 11,
+    name: "Solana",
+    chain: "SOL",
+    address: "0123456789abcdef0123456789abcdef01234567",
+    verified: true,
+  },
+  {
+    id: 12,
+    name: "Ethereum",
+    chain: "ETH",
+    address: "0123456789abcdef0123456789abcdef01234567",
+    verified: false,
+  },
+  {
+    id: 13,
+    name: "Ethereum",
+    chain: "ETH",
+    address: "0123456789abcdef0123456789abcdef01234567",
+    verified: true,
+  },
+  {
+    id: 14,
+    name: "Ethereum",
+    chain: "ETH",
+    address: "0123456789abcdef0123456789abcdef01234567",
+    verified: false,
+  },
 ];
 
 const TokenList = () => {
@@ -42,30 +127,61 @@ const TokenList = () => {
 
   return (
     <DefaultLayout>
-      <div className="container mx-auto p-4 h-full">
+      <div className="container mx-auto h-full p-4">
         {/* Header */}
-        <h2 className="text-2xl font-bold mb-6 text-white flex items-center gap-2"> <FaList/> Token List</h2>
+        <h2 className="mb-6 flex items-center gap-2 text-2xl font-bold text-white">
+          {" "}
+          <FaList /> Token List
+        </h2>
 
         {/* Table */}
         <div className="overflow-x-auto border  shadow-md">
           <table className="min-w-full bg-[#191919] text-white">
             <thead>
               <tr className="bg-[#191919]">
-                <th className="text-left p-4 border-b border-r border-[ #4C4C4C]">No</th>
-                <th className="text-left p-4 border-b border-r border-[ #4C4C4C]">Token Name</th>
-                <th className="text-left p-4 border-b border-r border-[ #4C4C4C]">Token Chain</th>
-                <th className="text-left p-4 border-b border-r border-[ #4C4C4C]">Token Address</th>
-                <th className="text-left p-4 border-b border-r border-[ #4C4C4C]">Status</th>
+                <th className="border-[ #4C4C4C] border-b border-r p-4 text-left">
+                  {" "}
+                  <div className="flex items-center justify-between gap-4">
+                    {" "}
+                    No <RiExpandUpDownFill />
+                  </div>
+                </th>
+                <th className="border-[ #4C4C4C] border-b border-r p-4 text-left">
+                  <div className="flex items-center justify-between gap-4">
+                    Token Name <RiExpandUpDownFill />
+                  </div>
+                </th>
+                <th className="border-[ #4C4C4C] border-b border-r p-4 text-left">
+                  <div className="flex items-center justify-between gap-4">
+                    Token Chain <RiExpandUpDownFill />
+                  </div>
+                </th>
+                <th className="border-[ #4C4C4C] border-b border-r p-4 text-left">
+                  <div className="flex items-center justify-between gap-4">
+                    Token Address <RiExpandUpDownFill />
+                  </div>
+                </th>
+                <th className="border-[ #4C4C4C] border-b border-r p-4 text-center">
+                  Status
+                </th>
               </tr>
             </thead>
             <tbody>
               {currentItems.map((token, index) => (
-                <tr key={token.id} className="border-t border-[ #4C4C4C]">
-                  <td className="p-4 border-r border-[#4C4C4C]">{indexOfFirstItem + index + 1}</td>
-                  <td className="p-4 border-r border-[#4C4C4C]">{token.name}</td>
-                  <td className="p-4 border-r border-[#4C4C4C]">{token.chain}</td>
-                  <td className="p-4 flex items-center border-r border-[#4C4C4C]">
-                    <span>{token.address.slice(0, 8)}...{token.address.slice(-8)}</span>
+                <tr key={token.id} className="border-[ #4C4C4C] border-t">
+                  <td className="border-r border-[#4C4C4C] p-4">
+                    {indexOfFirstItem + index + 1}
+                  </td>
+                  <td className="border-r border-[#4C4C4C] p-4">
+                    {token.name}
+                  </td>
+                  <td className="border-r border-[#4C4C4C] p-4">
+                    {token.chain}
+                  </td>
+                  <td className="flex items-center border-r border-[#4C4C4C] p-4">
+                    <span>
+                      {token.address.slice(0, 8)}...{token.address.slice(-8)}
+                    </span>
                     <button
                       className="ml-2 text-blue-400 underline"
                       onClick={() => copyToClipboard(token.address)}
@@ -74,17 +190,17 @@ const TokenList = () => {
                     </button>
                   </td>
                   <td className=" px-4  ">
-                <div  className=" flex justify-center items-center" >
-                {token.verified ? (
-                      <button className="bg-[#00FD81] rounded-xl px-6 py-2 justify-center font-semibold text-black-2 flex items-center">
-                        <RiCheckboxCircleFill className="mr-1" /> Verified
-                      </button>
-                    ) : (
-                      <button className="bg-[#FF3838] rounded-xl px-2 py-2 justify-center font-semibold text-white flex items-center">
-                        <RiCloseCircleFill className="mr-1" /> Not Verified
-                      </button>
-                    )}
-                </div>
+                    <div className=" flex items-center justify-center">
+                      {token.verified ? (
+                        <button className="flex items-center justify-center rounded-xl bg-[#00FD81] px-6 py-2 font-semibold text-black-2">
+                          <RiCheckboxCircleFill className="mr-1" /> Verified
+                        </button>
+                      ) : (
+                        <button className="flex items-center justify-center rounded-xl bg-[#FF3838] px-2 py-2 font-semibold text-white">
+                          <RiCloseCircleFill className="mr-1" /> Not Verified
+                        </button>
+                      )}
+                    </div>
                   </td>
                 </tr>
               ))}
@@ -93,9 +209,9 @@ const TokenList = () => {
         </div>
 
         {/* Pagination */}
-        <div className="flex justify-center items-center mt-4 space-x-2">
+        <div className="mt-4 flex items-center justify-center space-x-2">
           <button
-            className={`px-4 py-2 rounded-md ${currentPage === 1 ? "bg-gray-400" : "bg-blue-600 text-white"}`}
+            className={`rounded-md px-4 py-2 ${currentPage === 1 ? "bg-gray-400" : "bg-blue-600 text-white"}`}
             onClick={() => setCurrentPage(currentPage - 1)}
             disabled={currentPage === 1}
           >
@@ -105,7 +221,7 @@ const TokenList = () => {
             Page {currentPage} of {totalPages}
           </span>
           <button
-            className={`px-4 py-2 rounded-md ${currentPage === totalPages ? "bg-gray-400" : "bg-btn py-2 text-white"}`}
+            className={`rounded-md px-4 py-2 ${currentPage === totalPages ? "bg-gray-400" : "bg-btn py-2 text-white"}`}
             onClick={() => setCurrentPage(currentPage + 1)}
             disabled={currentPage === totalPages}
           >
