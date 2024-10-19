34cfa45576e0d34c6711cf52157f03dceb24c884
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-10-19 21:51:29 +0530

    rpc management done

--- src/app/rpc-url/save-url/page.tsx
+++ src/app/rpc-url/save-url/page.tsx
@@ -7,6 +7,10 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import RpcToast from "@/components/RpcToast";
 import { PiLinkSimpleDuotone } from "react-icons/pi";
+import { FaSyncAlt, FaCopy } from "react-icons/fa";
+import { CiCircleList } from "react-icons/ci";  // List icon
+import { FaRegEdit } from "react-icons/fa";    // Edit icon
+import { MdDelete } from "react-icons/md";     // Delete icon
 import dynamic from "next/dynamic";
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
 
@@ -18,7 +22,7 @@ const AddRpcUrl: React.FC<{ onSave: () => void }> = ({ onSave }) => {
   const { address } = useAccount();
   const [rpcUrl, setRpcUrl] = useState<string>("");
   const [rpcName, setRpcName] = useState<string>("");
-  const RpcSave = () => toast("RPC URL Saved!");
+ 
   const handleSaveRpcUrl = useCallback(async () => {
     if (!rpcUrl || !rpcName) { RpcToast("Please provide both name and URL.", "warn"); return }
 
@@ -35,13 +39,13 @@ const AddRpcUrl: React.FC<{ onSave: () => void }> = ({ onSave }) => {
   }, [rpcUrl, rpcName, address, onSave]);
 
   return (
-    <div className="bg-gray-100 shadow-md dark:bg-[#191919] px-4 py-4 rounded-md mb-6">
-      <h1 className="text-2xl flex items-center gap-2 font-bold dark:text-white">
+    <div className="bg-gray-300 shadow-md dark:bg-[#191919] px-4 py-6 rounded-md mb-6">
+      <h1 className="text-2xl flex items-center gap-2 font-bold dark:text-white text-black-2">
         <PiLinkSimpleDuotone /> ADD RPC URL
       </h1>
-      <div className="flex items-center gap-4 mb-4">
+      <div className="flex items-center gap-4 my-4">
         <div className="w-full sm:w-1/2">
-          <label className="dark:text-white">RPC Name</label>
+          <label className="dark:text-white text-black font-medium">RPC Name</label>
           <input
             type="text"
             className="w-full p-2 border border-gray-300 rounded-md dark:bg-[#191919]"
@@ -62,10 +66,10 @@ const AddRpcUrl: React.FC<{ onSave: () => void }> = ({ onSave }) => {
         </div>
       </div>
       <button
-        className="bg-primary-gradient text-white py-2 px-4 rounded-md w-full"
+        className="bg-primary-gradient text-white py-2 px-4 rounded-md w-full font-semibold"
         onClick={handleSaveRpcUrl}
       >
-        Submit RPC
+        SAVE RPC
       </button>
     </div>
   );
@@ -103,20 +107,52 @@ const RpcTable: React.FC<{
 
   return (
     <div className="mt-4">
-      <div className="flex justify-between mb-4">
-        <h2 className="text-xl font-semibold">RPC URLs</h2>
-        <div>
-          <button className="bg-blue-500 text-white py-1 px-3 rounded-md mr-2" onClick={onRefresh}>
-            Reload
-          </button>
-          <button className="bg-purple-500 text-white py-1 px-3 rounded-md">Copy</button>
+     
+      <div className="flex items-center justify-between mb-4 p-2 bg-gray-100 dark:bg-[#191919] rounded-lg shadow-md">
+      {/* Dropdown for Chain Selection */}
+      <div className="relative flex items-center w-full sm:w-1/2">
+        <select className="appearance-none block w-full px-4 py-2 pr-8 bg-gray-300 dark:bg-[#191919] text-white border border-gray-600 rounded-md focus:outline-none focus:ring focus:border-blue-300">
+          <option value="ETH">ETH</option>
+          <option value="BTC">BTC</option>
+          <option value="BSC">BSC</option>
+          <option value="Polygon">Polygon</option>
+        </select>
+        <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-600">
+          <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+            <path d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" />
+          </svg>
         </div>
+      </div>
+
+      {/* Buttons */}
+      <div className="flex gap-2">
+        {/* Reload Button */}
+        <button
+          className="flex items-center gap-2 px-4 py-2 btn-rest text-white  rounded-md  font-semibold hover:bg-purple-700"
+          onClick={onRefresh}
+        >
+          <FaSyncAlt />
+          Reload
+        </button>
+
+        {/* Copy Button */}
+        <button
+          className="flex items-center gap-2 btn-rest text-white px-4 py-1 rounded-md  font-semibold hover:bg-purple-700"
+        
+        >
+          <FaCopy />
+          Copy
+        </button>
+
+    </div>
+      
+     
       </div>
       {loading ? (
         <Loader />
       ) : (
-        <table className="min-w-full bg-gray-100 shadow-md  dark:bg-[#191919] rounded-md overflow-hidden border  border-[#434C59]">
-          <thead>
+        <table className="min-w-full bg-gray-100 shadow-md  dark:bg-[#191919]  overflow-hidden border  border-[#434C59]">
+          <thead className="  border-[#434C59]">
             <tr className="bg-gray-100 shadow-md  dark:bg-[#191919] border-[#434C59] border">
               <th className="text-left p-4 border border-[#434C59]">No</th>
               <th className="text-left p-4 border border-[#434C59]">RPC URLs</th>
@@ -299,26 +335,32 @@ const SaveUrls: React.FC = () => {
     <DefaultLayout>
       <div className="w-full p-4">
         <AddRpcUrl onSave={handleListRpcUrls} />
-        <div className="flex gap-4 mb-6">
-          <button
-            className={`px-4 py-2 rounded-md ${activeTab === "list" ? "bg-primary-gradient text-white" : "bg-gray-200 dark:bg-[#2f2f2f] dark:text-white"}`}
-            onClick={() => setActiveTab("list")}
-          >
-            List RPC URLs
-          </button>
-          <button
-            className={`px-4 py-2 rounded-md ${activeTab === "edit" ? "bg-primary-gradient text-white" : "bg-gray-200 dark:bg-[#2f2f2f] dark:text-white"}`}
-            onClick={() => setActiveTab("edit")}
-          >
-            Edit RPC URLs
-          </button>
-          <button
-            className={`px-4 py-2 rounded-md ${activeTab === "delete" ? "bg-primary-gradient text-white" : "bg-gray-200 dark:bg-[#2f2f2f] dark:text-white"}`}
-            onClick={() => setActiveTab("delete")}
-          >
-            Delete RPC URLs
-          </button>
-        </div>
+      <div className="bg-gray-300 shadow-md  dark:bg-[#191919] rounded-md  py-4 px-4 ">
+      <div className="flex gap-4 mb-6  border-b border-gray-400">
+      {/* List RPC URLs Tab */}
+      <button
+        className={`px-4 py-4 flex items-center gap-2 font-semibold  ${activeTab === "list" ? "active-rpc text-white" : " text-black-2 dark:text-white "}`}
+        onClick={() => setActiveTab("list")}
+      >
+        <CiCircleList className="text-xl" /> List RPC URLs
+      </button>
+
+      {/* Edit RPC URLs Tab */}
+      <button
+        className={`px-4 py-2 flex items-center gap-2 font-semibold  ${activeTab === "edit" ? "active-rpc text-white" : " text-black-2 dark:text-white"}`}
+        onClick={() => setActiveTab("edit")}
+      >
+        <FaRegEdit className="text-xl" /> Edit RPC URLs
+      </button>
+
+      {/* Delete RPC URLs Tab */}
+      <button
+        className={`px-4 py-2 flex items-center gap-2 font-semibold  ${activeTab === "delete" ? "active-rpc text-white" : " text-black-2 dark:text-white"}`}
+        onClick={() => setActiveTab("delete")}
+      >
+        <MdDelete className="text-xl" /> Delete RPC URLs
+      </button>
+    </div>
 
         <RpcTable
           rpcList={rpcList}
@@ -329,6 +371,7 @@ const SaveUrls: React.FC = () => {
           loading={loading}
           onRefresh={handleListRpcUrls}
         />
+      </div>
       </div>
       <ToastContainer />
     </DefaultLayout>
