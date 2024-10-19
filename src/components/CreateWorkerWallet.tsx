6b2ddeac397d320e6defd83d1181b4e5970bab5f
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-10-20 02:54:13 +0530

    Update CreateWorkerWallet.tsx

--- src/components/CreateWorkerWallet.tsx
+++ src/components/CreateWorkerWallet.tsx
@@ -1,17 +1,18 @@
 "use client";
-
 import React, { useState } from "react";
 import { createWorkerWallet } from "../utils/api";
 import { FaListUl } from "react-icons/fa";
 import { IoSettings } from "react-icons/io5";
-import { MdAddToPhotos } from "react-icons/md";
 import { useAccount } from "wagmi";
+import ListWallets from "../components/Listwallets";
+import SetWalletType from "../components/SetWalletType";
 
 const CreateWorkerWallet = () => {
   const { address } = useAccount();
   const [numberOfWorkers, setNumberOfWorkers] = useState<number>(1);
   const [message, setMessage] = useState<string | null>(null);
   const [successMessage, setSuccessMessage] = useState<string | null>(null);
+  const [activeTab, setActiveTab] = useState<string>("list-wallets");
 
   // Function to handle creating a new worker wallet(s)
   const handleCreateWorkerWallet = async () => {
@@ -25,7 +26,9 @@ const CreateWorkerWallet = () => {
 
     try {
       const res = await createWorkerWallet(address, numberOfWorkers);
-      setSuccessMessage(`Worker Wallet(s) Created Successfully: ${res.data.message}`);
+      setSuccessMessage(
+        `Worker Wallet(s) Created Successfully: ${res.data.message}`,
+      );
       setNumberOfWorkers(1); // Reset the number of workers after creation
     } catch (error) {
       console.error(error);
@@ -34,59 +37,71 @@ const CreateWorkerWallet = () => {
   };
 
   return (
-    <div className=" mt-8">
-      <h1 className="text-2xl font-bold text-center">Worker Wallet Management</h1>
-
+    <div className="mt-2">
       {/* Create Worker Wallet Section */}
-      <div className=" flex justify-center gap-8 mt-8 ">
-        <div className="bg-white shadow-xl rounded-2xl dark:bg-[#1c1d32] px-4 py-8 border border-[#19f48a9f]  sm:w-[500px] ">
+      <div className="mt-2 flex flex-col gap-8">
+        <div className="w-full rounded-md bg-white px-4 py-8 shadow-xl dark:bg-[#191919]">
+          <h1 className="text-2xl font-bold text-black-2 dark:text-white">
+            Worker Wallet Management
+          </h1>
           <div className="mt-6">
-            <h2 className="text-xl font-semibold">Create Worker Wallets</h2>
+            <h2 className="mb-4 text-lg font-semibold text-black-2 dark:text-white">
+              Create Worker Wallets
+            </h2>
             <input
               type="number"
-              className=" p-2  mt-2 rounded w-full border border-[#19f48a9f] "
+              className="w-full rounded-md border border-[#434C59] bg-white p-2 shadow-lg dark:bg-[#191919]"
               placeholder="Number of Worker Wallets"
               value={numberOfWorkers}
-              onChange={e => setNumberOfWorkers(Number(e.target.value))}
+              onChange={(e) => setNumberOfWorkers(Number(e.target.value))}
               min={1}
             />
             <button
-              className="mt-4 bg-primary-gradient text-white px-8 py-2 rounded-full mx-auto flex justify-center font-semibold "
+              className="bg-primary-gradient mt-4 w-full rounded-md px-4 py-2 font-semibold text-white"
               onClick={handleCreateWorkerWallet}
             >
               Create Wallets
             </button>
 
             {message && <div className="mt-4 text-red-500">{message}</div>}
-            {successMessage && <div className="mt-4 text-green-500">{successMessage}</div>}
+            {successMessage && (
+              <div className="mt-4 text-green-500">{successMessage}</div>
+            )}
           </div>
         </div>
-        <div className=" flex flex-col  gap-4  ">
-          <a href="/worker-wallets" className=" flex items-center gap-3 font-semibold">
-            {" "}
-            <MdAddToPhotos /> Create Worker Wallets
-          </a>
 
-          <a href="/worker-wallets/list-worker-wallets" className=" flex items-center gap-3 font-semibold">
-            {" "}
-            <FaListUl />
-            List Worker Wallets
-          </a>
+        {/* Tab navigation */}
+        <div className="rounded-md bg-gray-300 px-4 py-4 shadow-md dark:bg-[#191919]">
+          <div className="flex flex-row gap-4">
+            <button
+              className={`flex items-center gap-3 font-semibold ${
+                activeTab === "list-wallets" ? "text-primary" : ""
+              }`}
+              onClick={() => setActiveTab("list-wallets")}
+            >
+              <FaListUl />
+              List Worker Wallets
+            </button>
 
-          <a href="/worker-wallets/set-wallet" className=" flex items-center gap-3 font-semibold">
-            {" "}
-            <IoSettings /> Set Wallet Types
-          </a>
+            <button
+              className={`flex items-center gap-3 font-semibold ${
+                activeTab === "set-wallet" ? "text-primary" : ""
+              }`}
+              onClick={() => setActiveTab("set-wallet")}
+            >
+              <IoSettings />
+              Set Wallet Types
+            </button>
+          </div>
+          <div className="mt-4">
+          {activeTab === "list-wallets" && <ListWallets />}
+          {activeTab === "set-wallet" && <SetWalletType />}
         </div>
+        </div>
+
+        {/* Tab content */}
+       
       </div>
-
-      {/* List Worker Wallets Section */}
-      {/* <WorkerWallets /> */}
-
-      {/* Send Token Section */}
-      {/* <div className="mt-6">
-        <SendToken />
-      </div> */}
     </div>
   );
 };
